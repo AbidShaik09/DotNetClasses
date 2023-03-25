8b1a9f83717ef0b3ff64cafa0f4fe652db33d0f7
AbidShaik09 <102479021+AbidShaik09@users.noreply.github.com>
2023-03-25 11:22:27 +0530

    FirstClass

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+Console.WriteLine("Hello, World!");
+var p1 = new Si(1200, 5, 4);
+Console.WriteLine("Calculated Simple intrest : "+p1.Calculate());
---
+++ ConsoleApp1/ConsoleApp1/SimpleIntrest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    
+    internal class Si{
+        internal int P;
+        internal float R;
+        internal int T;
+        public Si(int P,float R,int T)
+        {
+            this.P = P;
+            this.R = R; 
+            this.T = T;
+        }
+        public float Calculate()
+        {
+            float si = (P * T * R) / 100;
+            return si;
+        }
+    }
+}
