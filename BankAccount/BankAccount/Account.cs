7319257a7e1615fbb04816d40e44e573b31d79e3
AbidShaik09 <102479021+AbidShaik09@users.noreply.github.com>
2023-03-25 15:39:17 +0530

    Bank Account

---
+++ BankAccount/BankAccount/Account.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    internal class Account
+    {
+        double mainBalance;
+        String? ownerName;
+        
+
+        internal Account(string? ownerName,double mainBalance)
+        {
+            this.ownerName = ownerName;
+            this.mainBalance = mainBalance;
+            
+        }
+        public void withdraw(int x)
+        {
+            if (x > mainBalance)
+            {
+                Console.WriteLine("Error: Insufficient fund");
+            }
+            else
+            {
+                mainBalance -= x;
+                Console.WriteLine("Successful: Remaining Balance= "+mainBalance);
+            }
+        }
+        public void deposit(int x)
+        {
+            mainBalance += x;
+            Console.WriteLine(x+" Rs Deposited Successfully");
+
+        }
+        public void Enquiry()
+        {
+            Console.WriteLine("Account name : "+ownerName+"\nBalance: "+mainBalance);
+        }
+
+        /*  owner name, Main balance,remaining balance, withdraw ,deposit, main balance  */
+    }
+}
---
+++ BankAccount/BankAccount/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BankAccount;
+using System.Transactions;
+
+Console.WriteLine("Welcome to EPAM Bank:");
+Console.WriteLine("Enter owner name");
+string? name=Console.ReadLine();
+Console.WriteLine("Enter Initial Deposit Amount:");
+double amt= Console.Read();
+var u1 = new Account(name,amt);
+while (true)
+{
+    Console.WriteLine("Choose:\n0.Deposit\n1.Withdraw\n2.Enquiry");
+    int n = int.Parse(Console.ReadLine());
+    switch (n)
+    {
+        case 0:
+            Console.WriteLine("Enter Amount to be Deposited:");
+            int x = int.Parse(Console.ReadLine());
+            u1.deposit(x);
+            break;
+        case 1:
+            Console.WriteLine("Enter Amount to be Withdrawn:");
+            int x1 = int.Parse(Console.ReadLine());
+            u1.withdraw(x1);
+            break; 
+        case 2:
+            Console.WriteLine("Fetching User details:");
+            u1.Enquiry();
+            break;
+        default:
+            return;
+    }
+}
