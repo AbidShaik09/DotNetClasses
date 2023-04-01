ff1b1d5e99502339d8873087571369bc2679b378
AbidShaik09 <102479021+AbidShaik09@users.noreply.github.com>
2023-04-01 15:35:12 +0530

    Week 2 Project Calculator with exception handelling

---
+++ Calculator/Calculator/Cal.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Cal
+    {
+        internal int res;
+
+        internal int add(int x, int y)
+        {
+            res = x + y;
+            return res;
+        }
+        internal int sub(int x, int y)
+        {
+            res = x - y;
+            return res;
+        }
+        internal int mul(int x, int y) {
+            res = x * y;
+            return res;
+        }
+        internal double div(int x, int y)
+        {
+            if (y != 0)
+            {
+                res = x / y;
+                return x / y;
+            }
+            else
+            {
+                throw new DivideByZeroException();
+                
+            }
+
+        }
+        internal int mod(int x, int y)
+        {
+            res = x % y;
+            return res;
+        }
+
+    }
+
+    internal class MathCal: Cal
+    {
+        internal int add(int[] x) {
+            res = 0;
+           
+                foreach (int i in x)
+                {
+                    res += i;
+                }
+            return res;
+        }
+    }
+}
---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Calculator;
+var c = new MathCal();
+
+while (true)
+{
+    int x=0, y=0;
+    Console.WriteLine("press enter to start :");
+    Console.ReadLine();
+    Console.Clear();
+    Console.WriteLine("Choose operation:\n1.Add\n2.Subract\n3.Multiply\n4.Divide ");
+    int s = Int32.Parse(Console.ReadLine());
+    if (s != 1)
+    {
+        Console.Write("enter operands:\nn1 = ");
+        x = Int32.Parse(Console.ReadLine());
+        Console.Write("n2 = ");
+        y = Int32.Parse(Console.ReadLine());
+    }
+    
+
+
+
+    switch (s)
+    {
+        case (1):
+            Console.WriteLine("Enter space separated numbers");
+            String[] r=(Console.ReadLine().Split(" "));
+            try
+            {
+                int[] a = Array.ConvertAll(r, int.Parse);
+                Console.WriteLine(c.add(a));
+            }
+            catch
+            {
+                Console.WriteLine("Numbers too big");
+            }
+            break;
+        case (2):
+            Console.WriteLine(c.sub(x, y)); break;
+        case (3):
+            Console.WriteLine(c.mul(x, y)); break;
+        case (4):
+            Console.WriteLine(c.div(x, y)); break;
+        case (5):
+            Console.WriteLine(c.mod(x, y)); break;
+        default: Console.WriteLine("Invalid operator"); break;
+    }
+
+}
